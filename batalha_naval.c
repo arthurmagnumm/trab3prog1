2eedddc5396808d496816d4d6afb49516bde1c78
arthurmagnumm <arthurmagnumm@gmail.com>
2015-11-08 23:31:06 +0000

    Concluido

--- batalha_naval.c
+++ batalha_naval.c
@@ -1,49 +1,106 @@
+/**************************************
+* Trabalho 3 de Programacao de Computadores I - Batalha Naval  
+* Curso de Sistemas da informacao  
+* Aluno: Arthur Magnum Porto dos Santos - matricula: 0050013432 
+* Professor: Alex Salgado 
+***************************************/
+
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
-    int mapa [5][5] = {{0,1,1,0,1},
-						{1,1,1,1,0},
-						{1,0,0,0,1},
-						{0,0,1,1,0},
-						{1,0,1,0,1}};
-    char controle[5][5];
-    int i, j;
-    int score =0;
-    
-    for(i=0; i<5; i++){
-             for(j=0; j<5; j++){
+int main()
+{
+	int mapa [6][6] = { {0,0,0,0,0,0},
+	                    {0,0,1,1,0,1},
+						{0,1,1,1,1,0},
+						{0,1,0,0,0,1},
+						{0,0,0,1,1,0},
+						{0,1,0,1,0,1}};
+	int i, j;
+	char controle[6][6];
+	int continuar = 1;
+	int score = 0;
+	
+
+        for(i=1; i<6; i++){
+             for(j=1; j<6; j++){
                       controle[i][j] = '.';
                       printf("%c  ", controle[i][j]);
              }
              printf("\n");
     }
     
-    printf("Informe as coordenadas do tiro (linha | coluna): ");
-    scanf("%d %d", &i, &j);
-    
-    while(i>-1 && j>-1){
-               if(mapa[i][j] == 1)
-               {
-                    controle[i][j] = 'X';
-                    score +1;
-               }
-               else
-               {
-                    controle[i][j] = 'O';
-               }
-               for(i=0; i<5; i++)
-               { 
-                    for(j=0; j<5; j++)
+    //primeira vez obrigatória de jogar
+   
+   
+    printf("\nEntre com a linha:  ");
+    scanf ("%d", &i);
+    printf("\nEntre com a Coluna:  ");
+    scanf("%d", &j);
+    if(mapa[i][j] == 1)
+    {
+        controle[i][j] = 'X';
+        score ++;
+        printf("Bomba!!\n");
+        printf("Seu score atual é de %d pontos\n", score);
+    	printf("Deseja Continuar? 1 = Sim / 0= Não:");
+    	scanf("%d", &continuar);
+        
+    }
+    else
+    {
+        controle[i][j] = 'O';
+        printf("Agua!!, que pena\n");
+        printf("Seu score atual é de %d pontos\n", score);
+    	printf("Deseja Continuar? 1 = Sim / 0= Não:");
+    	scanf("%d", &continuar);
+    }
+    for(i=1; i<6; i++)
+        { 
+            for(j=1; j<6; j++)
+            {
+                printf("%c  ", controle[i][j]);
+            }
+            printf("\n");
+        }
+            
+   //Começa o loop    
+   while(continuar ==1)
+   {
+            printf("\nEntre com a linha:  ");
+            scanf ("%d", &i);
+            printf("\nEntre com a Coluna:  ");
+            scanf("%d", &j);
+            if(mapa[i][j] == 1)
+            {
+                controle[i][j] = 'X';
+                score ++;
+                printf("Bomba!!\n");
+                printf("Seu score atual é de %d pontos\n", score);
+            	printf("Deseja Continuar? 1 = Sim / 0= Não:");
+            	scanf("%d", &continuar);
+                
+            }
+            else
+            {
+                controle[i][j] = 'O';
+                printf("Agua!!, que pena\n");
+                printf("Seu score atual é de %d pontos\n", score);
+            	printf("Deseja Continuar? 1 = Sim / 0= Não:");
+            	scanf("%d", &continuar);
+            }
+            for(i=1; i<6; i++)
+                { 
+                    for(j=1; j<6; j++)
                     {
                         printf("%c  ", controle[i][j]);
                     }
-                        printf("\n");
-               }
-                printf("voce acertou %d\n", score);
-                printf("Informe as coordenadas do tiro (linha coluna): ");
-                scanf("%d %d", &i, &j);
-    }
-    
-    system("pause");
-return 0;
+                    printf("\n");
+                }
+                    
+            
+            
+            
+   }
+   printf("Essas foram suas jogadas!\n\nObrigado por Jogar\n\n");
+	return 0;
 }
